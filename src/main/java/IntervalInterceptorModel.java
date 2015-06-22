f2f8ebca95256ee7d12017b1bfae6b2ff929fd7c
Robin Verlangen <robin@us2.nl>
2015-06-22 18:13:52 +0200

    Begin with custom interval algo

--- src/main/java/IntervalInterceptorModel.java
+++ src/main/java/IntervalInterceptorModel.java
@@ -1,3 +1,7 @@
+import sun.nio.cs.Surrogate;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
@@ -8,11 +12,17 @@ public class IntervalInterceptorModel {
     private TreeMap<Long, Double> data;
     private double maxValue;
     private double minValue;
+    private boolean debugEnabled;
+    private boolean isTrained;
+    private double avg;
+    private double stdDev;
+    private int dataCount;
 
     public IntervalInterceptorModel() {
         data = new TreeMap<Long, Double>();
         maxValue = Double.MIN_VALUE;
         minValue = Double.MAX_VALUE;
+        debugEnabled = true;
     }
 
     public void addData(long ts, double val) {
@@ -23,17 +33,18 @@ public class IntervalInterceptorModel {
         if (val > maxValue) {
             maxValue = val;
         }
+        isTrained = false;
     }
 
-    public void train() {
+    public void train() throws Exception {
         // Calculate average and standard deviation, very low stddev will make this unusable algo (as there are no peaks/lows)
         double total = 0.0D;
-        long count = 0L;
+        dataCount = 0;
         for (double val : data.values()) {
             total += val;
-            count++;
+            dataCount++;
         }
-        double avg = total / (double)count;
+        avg = total / (double)dataCount;
 
         // Standard deviation
         double msqT = 0.0D;
@@ -41,16 +52,115 @@ public class IntervalInterceptorModel {
             double msq = Math.pow(val - avg, 2.0D);
             msqT += msq;
         }
-        double msqAvg = msqT / (double)count;
-        double stdDev = Math.sqrt(msqAvg);
-        System.out.println("avg " + avg);
-        System.out.println("stddev " + stdDev);
+        double msqAvg = msqT / (double)dataCount;
+        stdDev = Math.sqrt(msqAvg);
+        debug("avg " + avg);
+        debug("stddev " + stdDev);
+        if (stdDev == 0.0D) {
+            isTrained = true;
+            debug("Standard deviation zero, stop training");
+            return;
+        }
+
+        // Does this suite a simple regression? IF so, it is probably not an interval dataset, throw exception
+        // @todo check
+
+        // Scan intervals
+        double scanValue = maxValue;
+        int maxIterations = 10000;
+        double scanStep = (maxValue - minValue) / maxIterations;
+        debug("Scan step size " + scanStep);
+        HashMap<Long, Double> foundPairs = new HashMap<Long, Double>();
+        for (int i = 0; i <maxIterations; i++) {
+            scanValue -= scanStep;
+            foundPairs.clear();
+            for (Map.Entry<Long, Double> kv: data.entrySet()) {
+                // Ignore below scan value
+                if (kv.getValue() < scanValue) {
+                    continue;
+                }
+                foundPairs.put(kv.getKey(), kv.getValue());
+            }
+            // Need at least two peaks to establish an interval
+            int foundPairCount = foundPairs.size();
+            if (foundPairCount < 2) {
+                continue;
+            }
+
+            // Can not be majority of datapoints
+            if (foundPairCount >= (double)dataCount * 0.3) {
+                continue;
+            }
+
+            // Debug
+            debug("Pairs above scan value of " + scanValue + ": " + foundPairs.toString());
+
+            // Regular intervals?
+            long previousTs = -1L;
+            long previousTsDelta = -1L;
+            boolean regularIntervals = true;
+            for (Map.Entry<Long, Double> kv: foundPairs.entrySet()) {
+                long ts = kv.getKey();
+                if (previousTs > -1L) {
+                    long tsDelta = ts - previousTs;
+                    debug("ts delta " + tsDelta);
+                    if (previousTsDelta > -1L) {
+                        if (tsDelta != previousTsDelta) {
+                            regularIntervals = false;
+                            debug("TS deltas are not regular");
+                            break;
+                        }
+                    }
+                    previousTsDelta = tsDelta;
+                }
+                previousTs = ts;
+            }
+
+            // Regular?
+            if (regularIntervals) {
+                debug("Regular intervals " + foundPairs.toString());
+            }
+
+            // Skip forward in scanStep to below the lowest value of the current pairs
+            double minValFound = Double.MAX_VALUE;
+            for (double val : data.values()) {
+                if (val < minValFound) {
+                    minValFound = val;
+                }
+            }
+            scanValue = minValFound - scanStep;
+            debug("Forward scanValue to " + scanValue);
+
+            // Done?
+            if (scanValue < minValue) {
+                break;
+            }
+        }
 
 
         // @todo Implement interceptors
+
+        // Done
+        isTrained = true;
     }
 
-    public double predict(long ts) {
+    protected void debug(String msg) {
+        if (debugEnabled) {
+            System.out.println("[" + getClass().getSimpleName()+"] " + msg);
+        }
+    }
+
+    public double predict(long ts) throws Exception {
+        // Must be trained
+        if (!isTrained) {
+            throw new Exception("Not trained, call train()");
+        }
+
+        // IF there is no standard deviation, the next value is probably the average
+        if (stdDev == 0.0D) {
+            return avg;
+        }
+
         // @todo Implement
         return -1.0D;
     }
--- src/main/java/IntervalInterceptorTimeserieAnalyzer.java
+++ src/main/java/IntervalInterceptorTimeserieAnalyzer.java
@@ -1,3 +1,4 @@
+import com.sun.javafx.tools.resource.DeployResource;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 import java.util.ArrayList;
@@ -12,33 +13,37 @@ public class IntervalInterceptorTimeserieAnalyzer extends AbstractTimeserieAnaly
     public List<TimeserieOutlier> analyze(AbstractDataLoader dataLoader, HashMap<String, Timeseries> timeseries) {
         List<TimeserieOutlier> outliers = new ArrayList<TimeserieOutlier>();
         for (Map.Entry<String, Timeseries> kv : timeseries.entrySet()) {
-            // Get slope
-            IntervalInterceptorModel r = new IntervalInterceptorModel();
+            try {
+                // Get slope
+                IntervalInterceptorModel r = new IntervalInterceptorModel();
 
-            // Train regression
-            for (Map.Entry<Long, Double> tskv : kv.getValue().getDataTrain().entrySet()) {
-                long ts = tskv.getKey();
-                double val = tskv.getValue();
-                r.addData(ts, val);
-            }
-
-            // Train
-            r.train();
-
-            // @todo Check reliable
-
-            // Predict
-            double maxRelDif = 0.05;
-            for (Map.Entry<Long, Double> tskv : kv.getValue().getDataClassify().entrySet()) {
-                long ts = tskv.getKey();
-                double val = tskv.getValue();
-                double expectedVal = r.predict(ts);
-                double dif = expectedVal / val;
-                dataLoader.log(dataLoader.LOG_DEBUG, getClass().getSimpleName(), ts + " " + val + " " + expectedVal + " (dif " + dif + ")");
-                if (Math.abs(dif) < 1 - maxRelDif || Math.abs(dif) > 1 + maxRelDif) {
-                    TimeserieOutlier outlier = new TimeserieOutlier(this.getClass().getSimpleName(), tskv.getKey(), tskv.getValue(), -1, -1);
-                    outliers.add(outlier);
+                // Train regression
+                for (Map.Entry<Long, Double> tskv : kv.getValue().getDataTrain().entrySet()) {
+                    long ts = tskv.getKey();
+                    double val = tskv.getValue();
+                    r.addData(ts, val);
                 }
+
+                // Train
+                r.train();
+
+                // @todo Check reliable
+
+                // Predict
+                double maxRelDif = 0.05;
+                for (Map.Entry<Long, Double> tskv : kv.getValue().getDataClassify().entrySet()) {
+                    long ts = tskv.getKey();
+                    double val = tskv.getValue();
+                    double expectedVal = r.predict(ts);
+                    double dif = expectedVal / val;
+                    dataLoader.log(dataLoader.LOG_DEBUG, getClass().getSimpleName(), ts + " " + val + " " + expectedVal + " (dif " + dif + ")");
+                    if (Math.abs(dif) < 1 - maxRelDif || Math.abs(dif) > 1 + maxRelDif) {
+                        TimeserieOutlier outlier = new TimeserieOutlier(this.getClass().getSimpleName(), tskv.getKey(), tskv.getValue(), -1, -1);
+                        outliers.add(outlier);
+                    }
+                }
+            } catch (Exception e) {
+                dataLoader.log(dataLoader.LOG_ERROR, getClass().getSimpleName(), e.getMessage());
             }
         }
         return outliers;
