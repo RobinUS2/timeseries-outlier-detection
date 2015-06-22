6262863e0b7c351792d5fb3516a275ccd68ddc3a
Robin Verlangen <robin@us2.nl>
2015-06-22 20:48:08 +0200

    First draft, lots of issues

--- src/main/java/IntervalInterceptorModel.java
+++ src/main/java/IntervalInterceptorModel.java
@@ -1,5 +1,6 @@
 import sun.nio.cs.Surrogate;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,12 +18,15 @@ public class IntervalInterceptorModel {
     private double avg;
     private double stdDev;
     private int dataCount;
+    private long tsDelta;
+    private ArrayList<IntervalPattern> intervalPatterns;
 
     public IntervalInterceptorModel() {
         data = new TreeMap<Long, Double>();
         maxValue = Double.MIN_VALUE;
         minValue = Double.MAX_VALUE;
         debugEnabled = true;
+        intervalPatterns = new ArrayList<IntervalPattern>();
     }
 
     public void addData(long ts, double val) {
@@ -41,7 +45,7 @@ public class IntervalInterceptorModel {
         double total = 0.0D;
         dataCount = 0;
         long tsPrev = -1;
-        long tsDelta = -1L;
+        tsDelta = -1L;
         for (Map.Entry<Long, Double> kv:  data.entrySet()) {
             double val = kv.getValue();
 
@@ -87,6 +91,9 @@ public class IntervalInterceptorModel {
         // Does this suite a simple regression? IF so, it is probably not an interval dataset, throw exception
         // @todo check
 
+        // Clear found patterns list
+        intervalPatterns.clear();
+
         // Scan intervals
         double scanValue = maxValue;
         int maxIterations = 10000;
@@ -101,6 +108,13 @@ public class IntervalInterceptorModel {
                 if (kv.getValue() < scanValue) {
                     continue;
                 }
+
+                // Ignore any values around avg
+                if (kv.getValue() >= avg -(0.1 * stdDev) && kv.getValue() <= avg + (0.1*stdDev)) {
+                    continue;
+                }
+
+                // Options
                 foundPairs.put(kv.getKey(), kv.getValue());
             }
             // Need at least two peaks to establish an interval
@@ -120,10 +134,24 @@ public class IntervalInterceptorModel {
             // Regular intervals?
             long previousTs = foundPairs.firstKey() - tsDelta;
             long previousTsDelta = -1L;
-            boolean regularIntervals = true;
             long intervalStartTs = -1L; // Start timestamp
             long intervalLength = 0L; // Steps in the current interval
+            long lastIntervalEndTs = -1L;
+            String possibleIntervalKey = "";
+            HashMap<String, Integer> possibleIntervals = new HashMap<String, Integer>();
+            double minVal = Double.MAX_VALUE;
+            double maxVal = Double.MIN_VALUE;
             for (Map.Entry<Long, Double> kv: foundPairs.entrySet()) {
+                // Store min and max
+                double val = kv.getValue();
+                if (val < minVal) {
+                    minVal = val;
+                }
+                if (val > maxVal) {
+                    maxVal = val;
+                }
+
+                // Analyze timestamps
                 long ts = kv.getKey();
                 if (previousTs > -1L) {
                     // Delta compared to previous entry
@@ -136,8 +164,18 @@ public class IntervalInterceptorModel {
                         debug("chained to prev delta");
                     } else {
                         if (intervalStartTs > -1L) {
+                            // End
                             debug("end of chain, started at " + intervalStartTs + " length " + intervalLength);
                             endOfChain = true;
+
+                            // Option
+                            if (lastIntervalEndTs > -1L) {
+                                long timeBetweenIntervals = ts - intervalStartTs;
+                                possibleIntervalKey = "l" + intervalLength + "_i" + timeBetweenIntervals;
+                            }
+
+                            // Last end
+                            lastIntervalEndTs = ts;
 
                             // Reset
                             intervalStartTs = -1L;
@@ -146,7 +184,7 @@ public class IntervalInterceptorModel {
                     }
                     intervalLength++;
 
-                    // Timestamp for spannning points
+                    // Timestamp for spanning points
                     if (intervalStartTs == -1L) {
                         intervalStartTs = ts;
                     }
@@ -159,9 +197,13 @@ public class IntervalInterceptorModel {
                         if (previousTsDelta > -1L) {
                             // Check for irregular series, only if the serie ended
                             if (intervalTsDelta != previousTsDelta) {
-                                regularIntervals = false;
                                 debug("TS deltas are not regular, is " + intervalTsDelta + " was " + previousTsDelta);
-                                break;
+                            } else {
+                                // Regular delta, possible interval
+                                int tmp = possibleIntervals.getOrDefault(possibleIntervalKey, 0);
+                                tmp++;
+                                possibleIntervals.put(possibleIntervalKey, tmp);
+                                possibleIntervalKey = "";
                             }
                         }
                         previousTsDelta = intervalTsDelta;
@@ -170,9 +212,23 @@ public class IntervalInterceptorModel {
                 previousTs = ts;
             }
 
-            // Regular?
-            if (regularIntervals) {
-                debug("Regular intervals " + foundPairs.toString());
+            debug("Possible intervals " + possibleIntervals.toString());
+            if (possibleIntervals.size() > 0) {
+                // Pick the first most occuring one
+                int maxOccurence = Integer.MIN_VALUE;
+                String maxK = "";
+                for (Map.Entry<String, Integer> kv : possibleIntervals.entrySet()) {
+                    if (kv.getValue() > maxOccurence) {
+                        maxOccurence = kv.getValue();
+                        maxK = kv.getKey();
+                    }
+                }
+                String[] split = maxK.split("_");
+                int length = Integer.parseInt(split[0].substring(1));
+                int interval = Integer.parseInt(split[1].substring(1));
+                debug("Pattern found: length " + length + " with interval of " + interval);
+                IntervalPattern ip = new IntervalPattern(length, interval, minVal, maxVal, lastIntervalEndTs);
+                intervalPatterns.add(ip);
             }
 
             // Skip forward in scanStep to below the lowest value of the current pairs
@@ -190,9 +246,6 @@ public class IntervalInterceptorModel {
                 break;
             }
         }
-
-
-        // @todo Implement interceptors
 
         // Done
         isTrained = true;
@@ -215,7 +268,50 @@ public class IntervalInterceptorModel {
             return avg;
         }
 
-        // @todo Implement
-        return -1.0D;
+        // No patterns?
+        if (intervalPatterns.size() < 1) {
+            return Double.NaN;
+        }
+
+        // Iterate patterns
+        for (IntervalPattern ip : intervalPatterns) {
+            long tSinceLastOccurrence = ts - ip.lastIntervalEndTs;
+            long patternLength = (ip.length * tsDelta) + ip.interval;
+            int patternsMatched = (int)Math.floor(tSinceLastOccurrence / patternLength);
+            long normalizedTsinceLastOccurence = tSinceLastOccurrence - (patternsMatched * patternLength);
+            debug(tSinceLastOccurrence + " tSinceLastOccurrence");
+            debug(normalizedTsinceLastOccurence + " normalizedTsinceLastOccurence");
+            debug(patternLength + " patternLength");
+            debug(patternsMatched + " patternsMatched");
+            if (normalizedTsinceLastOccurence >= ip.interval && normalizedTsinceLastOccurence <= patternLength) {
+                return (ip.minVal + ip.maxVal)/2; // @todo Store avg in pattern and use that
+            }
+        }
+
+        // Unable to forecast, not a peak / no peaks detected
+        // @todo return value from simple regression without all the peaks
+        return avg;
+    }
+
+    private class IntervalPattern {
+        private final int length;
+        private final int interval;
+        private final double minVal;
+        private final double maxVal;
+        private final long lastIntervalEndTs;
+        private IntervalPattern(int length, int interval, double minVal, double maxVal, long lastIntervalEndTs) {
+            this.length = length;
+            this.interval = interval;
+            this.minVal = minVal;
+            this.maxVal = maxVal;
+            this.lastIntervalEndTs = lastIntervalEndTs;
+        }
+
+
+        public String toString() {
+            return getClass().getSimpleName()  + " length=" + length + " interval=" +interval + " minval=" + minVal + " maxval=" + maxVal + " lastend=" + lastIntervalEndTs;
+        }
+
+
     }
 }
