9d8ab7ea9831e33fda5021d5fe488b29cf157781
Robin Verlangen <robin@us2.nl>
2015-06-22 22:40:13 +0200

    Begin harmonic fitter for sinusoids

---
+++ src/main/java/HarmonicFitterTimeserieAnalyzer.java
@@ -0,0 +1,19 @@
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by robin on 21/06/15.
+ */
+public class HarmonicFitterTimeserieAnalyzer extends AbstractTimeserieAnalyzer implements ITimeserieAnalyzer {
+    public List<TimeserieOutlier> analyze(AbstractDataLoader dataLoader, HashMap<String, Timeseries> timeseries) {
+        List<TimeserieOutlier> outliers = new ArrayList<TimeserieOutlier>();
+        for (Map.Entry<String, Timeseries> kv : timeseries.entrySet()) {
+            // @todo
+        }
+        return outliers;
+    }
+}
